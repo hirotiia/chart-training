8654233b436c1aa25a1b1d7bc55abe5477378325
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-10-03 20:43:48 +0900

    :wrench: eslintでフォルダ名チェック

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 import path from 'path';
 
 const buildEslintCommand = (filenames) => {
-  return `next lint --fix --file ${filenames
+  return `next lint --no-cache --fix --file ${filenames
     .filter((f) => f.includes('/src/'))
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
