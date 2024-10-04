f5e2a175ad13246fa0019f6b36b06d28b3070b84
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-10-04 19:11:15 +0900

    :wrench: storybook導入

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-actions',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+};
+export default config;
---
+++ src/components/ui/button/button.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './';
+
+const meta: Meta<typeof Button> = {
+  title: 'UI/Button', // Storybook上で表示されるコンポーネント名
+  component: Button,
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['default', 'secondary', 'destructive', 'muted', 'accent'],
+    },
+    size: {
+      control: 'select',
+      options: ['default', 'sm', 'lg'],
+    },
+    rounded: {
+      control: 'select',
+      options: ['default', 'lg', 'md', 'sm'],
+    },
+    icon: {
+      control: 'text',
+    },
+    children: {
+      control: 'text',
+    },
+    href: {
+      control: 'text',
+    },
+    as: {
+      control: 'select',
+      options: ['button', 'a', 'div'],
+    },
+  },
+  args: {
+    variant: 'default',
+    size: 'default',
+    rounded: 'default',
+    children: 'Button',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {};
+export const WithIcon: Story = {
+  args: {
+    icon: '🔔',
+  },
+};
+
+export const LinkButton: Story = {
+  args: {
+    as: 'a',
+    href: '#',
+  },
+};
