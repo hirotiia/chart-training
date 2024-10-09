566382095186e34ff85e21485a1db70fea06c7a7
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-10-09 19:27:39 +0900

    :zap: TODO追記

--- src/features/charts/api/get-stock.tsx
+++ src/features/charts/api/get-stock.tsx
@@ -1,6 +1,7 @@
 import { api } from '@/lib/api-client';
 
 export const GetStock = async () => {
+  // TODO:API Routes経由でリクエストを送る前にTanstack Query経由でキャッシュの有無を確認する
   const response = await api.get<string>('/api/stock');
   return { response };
 };
