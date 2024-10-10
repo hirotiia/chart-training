5d336037140b34995ede558407da08f2dc76a104
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-10-10 15:02:16 +0900

    :zap: APIリクエストの準備

--- src/features/charts/api/get-stock.tsx
+++ src/features/charts/api/get-stock.tsx
@@ -1,5 +1,11 @@
 import { api } from '@/lib/api-client';
-
+/**
+ * @param symbol 銘柄コード
+ * @param resolution データ時間解像度
+ * @param from 開始日時
+ * @param to 終了日時
+ * @returns Reaponse
+ */
 export const GetStock = async () => {
   // TODO:API Routes経由でリクエストを送る前にTanstack Query経由でキャッシュの有無を確認する
   const response = await api.get<{ hello: string }>('/api/stock');
--- src/features/charts/components/chart-view.tsx
+++ src/features/charts/components/chart-view.tsx
@@ -1,6 +1,8 @@
 import { GetStock } from '../api/get-stock';
 
 export const ChartView = async () => {
+  // ランダムに銘柄キーを渡す
+  // ランダムに期間を指定、そこから一ヶ月間の期間を渡す
   const { response } = await GetStock();
   console.log(response);
   return <p className="text-foregroundDark">{response.hello}</p>;
