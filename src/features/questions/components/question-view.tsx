3e497a9ffac4a8691ca3cc3ff623ea10954464f4
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-10-10 17:15:25 +0900

    :zap: cahrts を questionに統合

--- src/features/questions/components/question-view.tsx
+++ src/features/questions/components/question-view.tsx
@@ -1,14 +1,28 @@
 import { Card } from '@/components/ui/card';
-import { ChartView } from '@/features/charts/components/chart-view';
+import { api } from '@/lib/api-client';
+import { getRandomDateRange } from '@/utils/get-random-date-range';
 
 type QuestionViewProps = {
+  id: string;
   title: string;
 };
 
-export const QuestionView = ({ title }: QuestionViewProps) => {
+export const QuestionView = async ({ id, title }: QuestionViewProps) => {
+  /** ランダムで銘柄コードを一つ取得する関数実行 */
+  const symbol = '7203.T';
+
+  /** ランダムで{from: ?? , to: ??}を返す関数実行 */
+  const { from, to } = getRandomDateRange();
+  console.log(symbol, from, to);
+
+  /** クライアントキャッシュの確認、HTTPリクエストを送る */
+  const { hello } = await api.get<{ hello: string }>('/api/stock');
+
   return (
     <Card title={title}>
-      <ChartView></ChartView>
+      <p className="text-foregroundDark">{id}</p>
+      <p className="text-foregroundDark">{hello}</p>
+      {hello}
     </Card>
   );
 };
